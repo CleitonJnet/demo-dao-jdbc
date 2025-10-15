fd912fc028a930aee19e84a30e0cf1423da1c392
CleitonJnet <cleiton.jnet@gmail.com>
2025-10-15 13:42:49 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,7 +36,13 @@ public class Program {
         System.out.println("=== TEST 4: Seller insert =====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
-
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println();
+        System.out.println("=== TEST 5: Seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
     }
 }
